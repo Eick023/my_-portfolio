c64b830e4841886b0697414c4e6db9c77be5081b
Eick023 <ya5819@naver.com>
2024-07-12 16:30:01 +0900

    re_my_Portfolio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-const TextMain = document.querySelector('.logo');
+const introText = document.querySelectorAll("span");
+console.log(introText);
 
+window.onload = () => {
+  let timer = 100;
+  introText.forEach((item) => {
+    item.style.animation = `fade 500ms ${(timer += 50)}ms forwards`;
+  });
+};
